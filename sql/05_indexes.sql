9139602d049036194526136053898bdc216b1ff2
Abdullah Khan <abdullahsa.tech@gmail.com>
2026-09-05 23:04:22 +0500

    Update 05_indexes.sql

--- sql/05_indexes.sql
+++ sql/05_indexes.sql
@@ -1,1 +1,10 @@
+-- Reconstructed from the surviving academic project documentation.
 
+CREATE INDEX idx_book_title
+ON BOOKS(title);
+
+CREATE INDEX idx_member_name
+ON MEMBERS(member_name);
+
+CREATE INDEX idx_issue_date
+ON ISSUEDBOOKS(issue_date);
